01c2347bb83c89ac70ae42eb924217c97b50db02
mathonrupinder <mathonrupinder97@gmail.com>
2020-09-21 22:42:53 +0530

    UC1:Employee Attendance Check is Added

--- employeeWage.java
+++ employeeWage.java
@@ -3,6 +3,17 @@ class employeeWage
 	public static void main(String args[])
 	{
 	System.out.println("Welcome to Employee Wage Computation Program");
-	}
+	int random=(int)Math.round(Math.random());
+        {
+                if (random==1)
+                {
+                System.out.println("Employee is present");
+                }
+                else
+                {
+                 System.out.println("Employee is absent");
+                }
+        }
+        }
 }
 
