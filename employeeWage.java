9bbef16cf4b4a3a8baa65c2e5aa85b326d01fed1
mathonrupinder <mathonrupinder97@gmail.com>
2020-09-23 11:15:19 +0530

    UC7:Refactor the code using Classes And Object

--- employeeWage.java
+++ employeeWage.java
@@ -1,55 +1,58 @@
-import java.util.*;
-class employeeWage
+class Employee
 {
-        public static void main(String args[])
-        {
-        System.out.println("Welcome to Employee Wage Computation Program");
-        System.out.println("--------------------------------------------");
-        Scanner sc= new Scanner(System.in);
-        System.out.print("Enter Wage Per Hour- ");
-        int wagePerHour= sc.nextInt();
-        System.out.print("Enter Working Days in Month- ");
-	int workingDays= sc.nextInt();
-        System.out.print("Enter Working Hours in Month- ");
-        int workingHours= sc.nextInt();
-        int dailyWage=0;
-        int totalWage=0;
-        int employeeHours=0;
-        int days=0;
-        int workHours=0;
-        System.out.println("Day: No     workHours          DailyWage           TotalWage");
-        while (days<workingDays && workHours<workingHours)
-	{
-        int random=(int)Math.round(Math.random());
-        int empCheck=(int)Math.round(Math.random());
-                switch (random)
+public int Wage_Per_Hour,Daily_Wage,Employee_Hours,Working_Days,Working_Hours;
+public int Total_Wage=0;
+public int Days=0;
+public int Work_Hours=0;
+String Company_Name;
+public void calculation(int Wage_per_Hour,int Working_Days,int Working_Hours )
+{
+this.Wage_Per_Hour=Wage_per_Hour;
+this.Working_Days=Working_Days;
+this.Working_Hours=Working_Hours;
+while (Days<Working_Days && Work_Hours<Working_Hours)
+{
+int Attendance_Check=(int)Math.round(Math.random());    //Employee Attendance Check
+int Employee_Check=(int)Math.round(Math.random());      //Employee PartTime FullTime Availability check
+switch (Attendance_Check)
                 {
-                case 1:
-
-                        switch (empCheck)
+                case 1:                                 //Is Present
+                        switch (Employee_Check)
                         {
-                        case 1:
-                                employeeHours=8;
+                        case 1:                         //Is FullTime
+                                Employee_Hours=8;
                                 break;
-                        case 0:
-                                employeeHours=4;
+                        case 0:                         //Is PartTime
+                                Employee_Hours=4;
                                 break;
                         default:
                                 break;
                         }
                 break;
-                case 0:
-                dailyWage=0;
+                case 0:                                 //Is Absent
+                Daily_Wage=0;
                 break;
                 default:
                 break;
                 }
-        days++;
-        workHours=employeeHours+workHours;
-        dailyWage=employeeHours*wagePerHour;
-        totalWage=dailyWage+totalWage;
-        System.out.println("Day  " +days+"        "+workHours+"                   "+dailyWage+"               "+totalWage);
-
-  }
+        Days++;
+        Work_Hours=Employee_Hours+Work_Hours;
+        Daily_Wage=Employee_Hours*Wage_Per_Hour;        //Daily Wage Calculation
+        Total_Wage=Total_Wage+Daily_Wage;               //Total Wage Calculation
+        System.out.println("Day  "+Days+"           "+Work_Hours+"              "+Daily_Wage+"             "+Total_Wage);
+}
+}
+}
+class employeeWage
+{
+public static void main(String args[])
+{
+System.out.println("*****Welcome To Employee Wage Computation Problem*****");
+Employee obj1=new Employee();
+obj1.Company_Name="COMPANY NAME::RELIANCE";
+System.out.println(obj1.Company_Name);
+System.out.println("---------------------------------");
+System.out.println("Day No.      Work_Hours      Daily_Wage     Total_Wage");
+obj1.calculation(20,20,100);
 }
 }
