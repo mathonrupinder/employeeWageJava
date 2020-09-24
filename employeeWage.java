a9d3e77f85b21acd85c1940d9fa945f2de374347
mathonrupinder <mathonrupinder97@gmail.com>
2020-09-24 12:09:31 +0530

    UC9_Storing total wage for each Company

--- employeeWage.java
+++ employeeWage.java
@@ -1,70 +1,87 @@
+import java.util.*;
 class Employee
 {
-public int Wage_Per_Hour,Daily_Wage,Employee_Hours,Working_Days,Working_Hours;
-public int Total_Wage=0;
-public int Days=0;
-public int Work_Hours=0;
-String Company_Name;
-public void calculation(int Wage_per_Hour,int Working_Days,int Working_Hours )
-{
-this.Wage_Per_Hour=Wage_per_Hour;
-this.Working_Days=Working_Days;
-this.Working_Hours=Working_Hours;
-while (Days<Working_Days && Work_Hours<Working_Hours)
-{
-int Attendance_Check=(int)Math.round(Math.random());    //Employee Attendance Check
-int Employee_Check=(int)Math.round(Math.random());      //Employee PartTime FullTime Availability check
-switch (Attendance_Check)
-                {
+	public int WAGE_PER_HOUR,WORKING_DAYS,WORKING_HOURS;
+	public int employeeHours;
+	public int TOTAL_WAGE=0;
+	public int DAILY_WAGE;
+	public int DAYS=0;
+	public int WORK_HOURS=0;
+	String Company_Name;
+	public void calculation()
+	{
+		while (DAYS<WORKING_DAYS && WORK_HOURS<WORKING_HOURS)
+	{
+		int Attendance_Check=(int)Math.round(Math.random());    //Employee Attendance Check
+		int Employee_Check=(int)Math.round(Math.random());      //Employee PartTime FullTime Availability check
+		DAYS++;
+                WORK_HOURS=employeeHours+WORK_HOURS;
+
+	switch (Attendance_Check)
+            {
                 case 1:                                 //Is Present
                         switch (Employee_Check)
                         {
                         case 1:                         //Is FullTime
-                                Employee_Hours=8;
+                                employeeHours=8;
                                 break;
                         case 0:                         //Is PartTime
-                                Employee_Hours=4;
+                                employeeHours=4;
                                 break;
                         default:
                                 break;
                         }
                 break;
                 case 0:                                 //Is Absent
-                Daily_Wage=0;
+                DAILY_WAGE=0;
                 break;
                 default:
                 break;
                 }
-        Days++;
-        Work_Hours=Employee_Hours+Work_Hours;
-        Daily_Wage=Employee_Hours*Wage_Per_Hour;        //Daily Wage Calculation
-        Total_Wage=Total_Wage+Daily_Wage;               //Total Wage Calculation
-        System.out.println("Day  "+Days+"           "+Work_Hours+"              "+Daily_Wage+"             "+Total_Wage);
+        DAILY_WAGE=(employeeHours*WAGE_PER_HOUR);              //Daily Wage Calculation
+        TOTAL_WAGE=(TOTAL_WAGE+DAILY_WAGE);                    //Total Wage Calculation
 }
 }
 }
 class employeeWage
 {
-public static void main(String args[])
-{
-System.out.println("*****Welcome To Employee Wage Computation Problem*****");
-Employee obj1=new Employee();
-obj1.Company_Name="COMPANY NAME::RELIANCE";
-System.out.println(obj1.Company_Name);
-System.out.println("---------------------------------");
-System.out.println("Day No.      Work_Hours      Daily_Wage     Total_Wage");
-obj1.calculation(20,20,100);
-Employee obj2=new Employee();
-obj2.Company_Name="COMPANY NAME::GOOGLE";
-System.out.println(obj2.Company_Name);
-System.out.println("---------------------------------");
-System.out.println("Day No.      Work_Hours      Daily_Wage     Total_Wage");
-obj2.calculation(30,25,150);
-Employee obj3=new Employee();
-obj3.Company_Name="COMPANY NAME::TATA CONSULTANCY SERVICES";
-System.out.println(obj3.Company_Name);
-System.out.println("---------------------------------");
-System.out.println("Day No.      Work_Hours      Daily_Wage     Total_Wage");
-obj3.calculation(15,25,120);
+
+	public static void main(String args[]) {
+	System.out.println("Welcome To Employee Wage Computation Problem");
+	Scanner sc= new Scanner(System.in);
+	//creating ARRAYLIST
+	ArrayList<Employee> employeeWageBuilder=new ArrayList<Employee>();
+	Scanner in = new Scanner(System.in);
+	System.out.println("Enter no. of companies");
+	int limit=sc.nextInt();
+	int i=0;
+	while (i!=limit)
+	{
+		i++;
+		Employee obj1=new Employee();
+		System.out.println("Enter Company Name");
+		obj1.Company_Name= in.nextLine();
+		System.out.println("Enter wage per hour");
+		obj1.WAGE_PER_HOUR= sc.nextInt();
+		System.out.println("Enter Working_Days");
+		obj1.WORKING_DAYS= sc.nextInt();
+		System.out.println("Enter Working_Hours");
+		obj1.WORKING_HOURS= sc.nextInt();
+		obj1.calculation();
+        
+	// Iterator
+	employeeWageBuilder.add(obj1);
+        Iterator itr=employeeWageBuilder.iterator();
+			while(itr.hasNext())
+			{
+				Employee emp=(Employee)itr.next();
+				System.out.println("---------------");
+				System.out.println(emp.Company_Name);
+				System.out.println("Total wage is "+emp.TOTAL_WAGE);
+				System.out.println("---------------");
+
+			}	
+	}
 }
-}
+
+
